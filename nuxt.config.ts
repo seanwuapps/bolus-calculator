5c333382bcc9b1aee48a04cd421482f32cd10849
Sean Wu <seanwuapps@gmail.com>
2023-11-11 08:28:32 +1100

    scaffold

---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ["@go-ui/vue/dist/go-ui.css"],
+});
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>hello</h1>
+  <ClientOnly>
+    <GoButton @click="test">TEST</GoButton>
+  </ClientOnly>
+  <div class="container">
+    <GoMd :content="content"></GoMd>
+  </div>
+</template>
+
+<script setup>
+import { GoMd, GoButton } from "@go-ui/vue";
+import content from "../README.md?raw";
+const test = () => {
+  console.log("test");
+};
+</script>
